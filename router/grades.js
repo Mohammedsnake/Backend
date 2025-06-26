ee33c5d54c2e86457aa9282a2deb392edc463f7c
LittleMO <153019785+Mohammedsnake@users.noreply.github.com>
2025-06-26 15:17:33 +0300

    Update grades.js

--- router/grades.js
+++ router/grades.js
@@ -1,45 +1,50 @@
 const router = require("express").Router();
 const dbconn = require("../config/dbconfig");
 
+// GET all grades
 router.get("/", (req, res) => {
-  dbconn.query("SELECT * FROM departments", (err, resp) => {
-    if (err) return res.send(err);
-    res.send(resp);
+  dbconn.query("SELECT * FROM grades", (err, result) => {
+    if (err) return res.status(500).send(err);
+    res.send(result);
   });
 });
 
+// GET a single grade by ID
 router.get("/:id", (req, res) => {
   const id = req.params.id;
-  dbconn.query("SELECT * FROM departments WHERE department_id = ?", [id], (err, resp) => {
-    if (err) return res.send(err);
-    res.send(resp);
+  dbconn.query("SELECT * FROM grades WHERE grade_id = ?", [id], (err, result) => {
+    if (err) return res.status(500).send(err);
+    res.send(result);
   });
 });
 
+// POST a new grade
 router.post("/", (req, res) => {
-  const { department_name, building_location } = req.body;
-  const sql = "INSERT INTO departments(department_name, building_location) VALUES (?, ?)";
-  dbconn.query(sql, [department_name, building_location], (err, resp) => {
-    if (err) return res.send(err);
-    res.send(resp);
+  const { student_id, course_id, grade, date_graded } = req.body;
+  const sql = "INSERT INTO grades (student_id, course_id, grade, date_graded) VALUES (?, ?, ?, ?)";
+  dbconn.query(sql, [student_id, course_id, grade, date_graded], (err, result) => {
+    if (err) return res.status(500).send(err);
+    res.send(result);
   });
 });
 
+// UPDATE an existing grade
 router.put("/:id", (req, res) => {
   const id = req.params.id;
-  const { department_name, building_location } = req.body;
-  const sql = "UPDATE departments SET department_name = ?, building_location = ? WHERE department_id = ?";
-  dbconn.query(sql, [department_name, building_location, id], (err, resp) => {
-    if (err) return res.send(err);
-    res.send(resp);
+  const { student_id, course_id, grade, date_graded } = req.body;
+  const sql = "UPDATE grades SET student_id = ?, course_id = ?, grade = ?, date_graded = ? WHERE grade_id = ?";
+  dbconn.query(sql, [student_id, course_id, grade, date_graded, id], (err, result) => {
+    if (err) return res.status(500).send(err);
+    res.send(result);
   });
 });
 
+// DELETE a grade by ID
 router.delete("/:id", (req, res) => {
   const id = req.params.id;
-  dbconn.query("DELETE FROM departments WHERE department_id = ?", [id], (err, resp) => {
-    if (err) return res.send(err);
-    res.send(resp);
+  dbconn.query("DELETE FROM grades WHERE grade_id = ?", [id], (err, result) => {
+    if (err) return res.status(500).send(err);
+    res.send(result);
   });
 });
 
